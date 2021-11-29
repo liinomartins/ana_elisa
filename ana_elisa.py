880c7b9a9edfd526328fd3d2fdcc76e9f7db4cb5
liinomartins <69821617+liinomartins@users.noreply.github.com>
2021-11-28 23:30:35 -0300

    Update ana_elisa.py

--- ana_elisa.py
+++ ana_elisa.py
@@ -7,7 +7,7 @@ df.rename(columns = {'V1':'Recency','V2':'Frequency','V3':'Monetary','V4':'Time'
 st.title('Ana Elisa Braz - Avaliação 2 - Módulo 2')
 st.write('Base Filtrada')
 st.dataframe(df)
-df['Target'].apply(str)
+df['Target'].astype(string)
 target = list(df['Target'].unique())
 target.append('Todas')
 targets = st.selectbox('Selecione o Target', options = target)
