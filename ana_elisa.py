e3b2514fc3124117ce5fbf6f00e7e28a95e29b9e
liinomartins <69821617+liinomartins@users.noreply.github.com>
2021-11-28 22:48:42 -0300

    Update ana_elisa.py

--- ana_elisa.py
+++ ana_elisa.py
@@ -4,8 +4,8 @@ import plotly.express as px
 
 df = pd.read_csv('https://www.openml.org/data/get_csv/1586225/php0iVrYT')
 df.rename(columns = {'V1':'Recency','V2':'Frequency','V3':'Monetary','V4':'Time','Class':'Target'}, inplace=True)
-st.title('Ana elisa teste da aplicacao')
-st.write('filtro e download do treco')
+st.title('Ana Elisa Braz - Avaliação 2 - Módulo 2')
+st.write('Base Filtrada')
 st.dataframe(df)
 target = list(df['Target'].unique())
 target.append('Todas')
@@ -33,3 +33,10 @@ def convert_df(df):
     return df.to_csv().encode('utf-8')   
 csv = convert_df(df)
 st.download_button(label = 'Download da base filtrada como CSV', data = csv, file_name = 'consulta.csv', mime = 'text/csv')
+
+# Download da tabela filtrada
+st.title('Download da Tabela Filtrada .json')
+def convert_df(df):
+    return df.to_json(orient = 'records')   
+json = convert_df(df)
+st.download_button(label = 'Download da base filtrada como JSON', data = json, file_name = 'consulta.json', mime = 'text/json')
